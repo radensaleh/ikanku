abb9fe950645833234e59040987405971b3bfc4d
sandypriyatna <sandypriyatna98@gmail.com>
2018-11-26 07:50:16 +0700

    Menu utama

---
+++ app/src/main/java/com/mafish/mafish/AddFishShopActivity.java
@@ -0,0 +1,124 @@
+package com.mafish.mafish;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class AddFishShopActivity extends AppCompatActivity {
+
+    EditText etFishName, etPrice, etDescription;
+    Button btnSelectImage, btnSubmit;
+    ImageView ivFish;
+
+    public static SQLiteHelper sqLiteHelper;
+    final static int REQUEST_CODE_GALLERY = 999;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_fish_shop);
+
+        etFishName = findViewById(R.id.et_fish_name);
+        etPrice = findViewById(R.id.et_price);
+        etDescription = findViewById(R.id.et_description);
+        ivFish = findViewById(R.id.iv_fish);
+        btnSelectImage = findViewById(R.id.btn_select_image);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        final SQLiteHelper sqLiteHelper = new SQLiteHelper(this, "FishDB.sqlite", null, 1);
+        sqLiteHelper.queryData("CREATE TABLE IF NOT EXISTS FISH (Id INTEGER PRIMARY KEY AUTOINCREMENT, fish_name VARCHAR, price VARCHAR, description VARCHAR, image BLOG)");
+
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(AddFishShopActivity.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY);
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    sqLiteHelper.insertData(
+                            etFishName.getText().toString().trim(),
+                            etPrice.getText().toString().trim(),
+                            etDescription.getText().toString().trim(),
+                            imageViewToByte(ivFish)
+                    );
+                    Toast.makeText(getApplicationContext(), "Added successfully!", Toast.LENGTH_SHORT).show();
+                    etFishName.setText("");
+                    etPrice.setText("");
+                    etDescription.setText("");
+                    ivFish.setImageResource(R.mipmap.ic_launcher);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GALLERY) {
+            if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setType("image/*");
+                startActivityForResult(i, REQUEST_CODE_GALLERY);
+            }
+            else {
+                Toast.makeText(this, "Anda tidak memiliki akses!", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                ivFish.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static byte[] imageViewToByte(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+}
---
+++ app/src/main/java/com/mafish/mafish/FishShopActivity.java
@@ -0,0 +1,13 @@
+package com.mafish.mafish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FishShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fish_shop);
+    }
+}
